f089f77936e038485b1375bb7683bbcf37344882
7environment <artemmartynenko485@gmail.com>
2025-09-16 02:54:45 +1000

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,269 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local UserInputService = game:GetService("UserInputService")
+local LocalPlayer = game:GetService("Players").LocalPlayer
+
+local Window = Rayfield:CreateWindow({
+    Name = "0iq's Destruction simulator GUI",
+    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+    LoadingTitle = "Destruction simulator",
+    LoadingSubtitle = "by 0iq",
+    ShowText = "0iq's gui", -- for mobile users to unhide rayfield, change if you'd like
+    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+    DisableRayfieldPrompts = false,
+    DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = nil, -- Create a custom folder for your hub/game
+        FileName = "Big Hub"
+    },
+
+    Discord = {
+        Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+        Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+        Title = "Untitled",
+        Subtitle = "Key System",
+        Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+        SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+        GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+        Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+})
+
+local MainTab = Window:CreateTab("Main tab", 4483362458) -- Title, Image
+
+local Helpful = MainTab:CreateSection("Helpful functions")
+
+local SellButton = MainTab:CreateButton({
+    Name = "Sell",
+    Callback = function()
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("sellBricks"):FireServer()
+    end,
+})
+
+local AutoSellBool
+local AutoSellToggle = MainTab:CreateToggle({
+    Name = "Auto Sell",
+    CurrentValue = false,
+    Flag = "AutoSellToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        AutoSellBool = Value
+        while AutoSellBool do
+            task.wait(.2)
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("sellBricks"):FireServer()
+        end
+    end,
+})
+
+local GunOrBombSection = MainTab:CreateSection("Choose explode")
+
+local GunOrBomb = "Gun"
+local GunOrBombDropdown = MainTab:CreateDropdown({
+    Name = "Gun or Bomb",
+    Options = {"Gun","Bomb"},
+    CurrentOption = {"Gun"},
+    MultipleOptions = false,
+    Flag = "GunOrBombDropdown", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Options)
+        GunOrBomb = Options[1]
+    end,
+})
+
+local SelfExplodeSection = MainTab:CreateSection("Self Explode Section (gun/bomb in your hand required)")
+
+local SelfExplodeCooldown = 500
+local SelfExplodeSlider = MainTab:CreateSlider({
+    Name = "Explode Cooldown",
+    Range = {0, 5000},
+    Increment = 10,
+    Suffix = "Milliseconds",
+    CurrentValue = 500,
+    Flag = "SelfExplodeSlider", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        SelfExplodeCooldown = Value
+    end,
+})
+
+local SelfExplodeBool = false
+local SelfExplodeToggle = MainTab:CreateToggle({
+    Name = "Explodes Yourself",
+    CurrentValue = false,
+    Flag = "SelfExplodeToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        SelfExplodeBool = Value
+        while SelfExplodeBool do
+            task.wait(SelfExplodeCooldown)
+            if GunOrBomb == "Gun" then
+                local args = {
+                    tick(),
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("Launcher"):WaitForChild("Stats"),
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("HumanoidRootPart").Position,
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("Launcher"):WaitForChild("Assets"):WaitForChild("Rocket"):WaitForChild("Boom")
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("explodeRocket"):FireServer(unpack(args))
+            else
+                local args = {
+                    tick(),
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("Bomb"):WaitForChild("Stats"),
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("HumanoidRootPart").Position
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("explodeBomb"):FireServer(unpack(args))
+            end
+        end
+    end,
+})
+
+local CursorExplodeSection = MainTab:CreateSection("Explode Cursor Section (gun/bomb in your hand required)")
+
+local CursorExplodeCooldown = 500
+local CursorExplodeSlider = MainTab:CreateSlider({
+    Name = "Explode Cooldown",
+    Range = {0, 5000},
+    Increment = 10,
+    Suffix = "Milliseconds",
+    CurrentValue = 500,
+    Flag = "CursorExplodeSlider", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        CursorExplodeCooldown = Value
+    end,
+})
+
+local CursorExplodeBool = false
+local CursorExplodeToggle = MainTab:CreateToggle({
+    Name = "Explodes Cursor",
+    CurrentValue = false,
+    Flag = "CursorExplodeToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        SelfExplodeBool = Value
+        while SelfExplodeBool do
+            task.wait(CursorExplodeCooldown)
+            local mousePosition = UserInputService:GetMouse().Hit.Position
+            if GunOrBomb == "Gun" then
+                local args = {
+                    tick(),
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("Launcher"):WaitForChild("Stats"),
+                    mousePosition,
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("Launcher"):WaitForChild("Assets"):WaitForChild("Rocket"):WaitForChild("Boom")
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("explodeRocket"):FireServer(unpack(args))
+            else
+                local args = {
+                    tick(),
+                    LocalPlayer:WaitForChild("Character"):WaitForChild("Bomb"):WaitForChild("Stats"),
+                    mousePosition
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("explodeBomb"):FireServer(unpack(args))
+            end
+        end
+    end,
+})
+
+local MiscTab = Window:CreateTab("Misc tab", 4483362458) -- Title, Image
+
+local GetAllBoosts = MiscTab:CreateButton({
+    Name = "Button Example",
+    Callback = function()
+        local args = {
+            "CoinBoost",
+            86400,
+            2
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+
+        local args = {
+            "SuperJump",
+            86400,
+            2
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+
+        local args = {
+            "ExtraSpeed",
+            86400,
+            2
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+
+        local args = {
+            "BrickBoost",
+            86400,
+            2
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+
+        local args = {
+            "XPBoost",
+            86400,
+            2
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+    end,
+})
+
+local Get99MCoins = MiscTab:CreateButton({
+    Name = "Get 99M Coins",
+    Callback = function()
+        local args = {
+            "Coins",
+            480,
+            99999999
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+    end,
+})
+
+local autoMoneyGen = false
+local autoMoneyGenToggle = MiscTab:CreateToggle({
+    Name = "Auto Money Generation",
+    CurrentValue = false,
+    Flag = "autoMoneyGenToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        autoMoneyGen = Value
+        while autoMoneyGen do
+            task.wait(.2)
+            local args = {
+                "Coins",
+                480,
+                99999999
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+        end
+    end,
+})
+
+local PlayerTab = Window:CreateTab("Player tab", 4483362458) -- Title, Image
+
+local SpeedSlider = PlayerTab:CreateSlider({
+    Name = "Player Speed ",
+    Range = {0, 200},
+    Increment = 1,
+    Suffix = "Bananas",
+    CurrentValue = 16,
+    Flag = "SpeedSlider", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        LocalPlayer:WaitForChild("Character"):WaitForChild("Humanoid").WalkSpeed = Value
+    end,
+})
+
+local JumpSlider = PlayerTab:CreateSlider({
+    Name = "Jump Power",
+    Range = {0, 200},
+    Increment = 1,
+    Suffix = "Bananas",
+    CurrentValue = 50,
+    Flag = "JumpSlider", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        LocalPlayer:WaitForChild("Character"):WaitForChild("Humanoid").JumpPower = Value
+        LocalPlayer:WaitForChild("Character"):WaitForChild("Humanoid").UseJumpPower = true
+    end,
+})
